db7b083c6fde3983d25e5a97e50c393c3c431bb7
walidbelharazem <walidbelharazem@gmail.com>
2023-09-11 10:44:00 +0200

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,24 @@
-import React from "react";
+/* import React from "react";
 import howManyParks from "./parks/howManyParks";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
+} */
+
+import React from "react";
+import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      
+      <MesaVerde/>
+      
+    </div>
+  );
 }
+
